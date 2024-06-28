a4c050f879d55558bdac92a34d50b18b07f8d073
Rifando Panggabean <fandofast@gmail.com>
2024-06-29 02:56:58 +0700

    f

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express()
 const AWS = require('aws-sdk')
 require('dotenv').config()
 
+
+// import { FullStaticSearch } from 'full-static-search'
+const FullStaticSearch = require('full-static-search').FullStaticSearch;
+const MiniSearch = require('minisearch')
+
+
 const s3 = new AWS.S3({
   endpoint: 'http://s3.ap-northeast-1.wasabisys.com',
     accessKeyId: process.env.ACCESSKEY, // Your AWS Access Key ID
@@ -14,10 +20,10 @@ const s3 = new AWS.S3({
 
 
 
-app.get('/d/:key', async (req, res) => {
+app.get('/d/', async (req, res) => {
   const params = {
     Bucket: process.env.MOVIEBUCKET, // Bucket name
-    Key: req.params.key, // File name you want to save as in S3
+    Key: req.query.key, // File name you want to save as in S3
     Expires: 15000, // 60 seconds is the default value, change if you want
   }
   const downloadURL = await s3.getSignedUrlPromise('getObject', params)
@@ -32,9 +38,80 @@ app.get('/d/:key', async (req, res) => {
 
 })
 
+
+app.get('/all/', async (req, res) => {
+  var bucketParams = {
+    Bucket : 'movie1111',
+    MaxKeys:5000,
+};
+
+// Call S3 to obtain a list of the objects in the bucket
+s3.listObjects(bucketParams, function(err, data) {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log("Success", data);
+        res.send({data});
+    }
+});
+
+})
+
+
+app.get('/search/', async (req, res) => {
+  var bucketParams = {
+    Bucket : 'movie1111',
+    MaxKeys:6000,
+};
+
+// Call S3 to obtain a list of the objects in the bucket
+s3.listObjects(bucketParams, function(err, data) {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        // console.log("Success", data);
+        // var containstext = data.Contents.filter(word => word.Key.toLowerCase().includes(req.query.q.toLowerCase()));
+       
+        // search.filter(req.query.q.toLowerCase())
+
+        var mykey=[]
+        var index=0;
+        data.Contents.forEach(element => {
+          mykey.push({'id':index++,
+            'title' :element.Key})
+        });
+
+        let miniSearch = new MiniSearch({
+          fields: ['title'], // fields to index for full-text search
+          storeFields: ['title'] 
+        })
+
+        console.log(mykey);
+
+        miniSearch.addAll(mykey)
+
+        let results = miniSearch.search(req.query.q)
+
+
+        console.log(results);
+
+        // console.log(data.Contents)
+        
+        // results.forEach( function (result) {
+        //   console.log(result.entry);
+        // });
+
+        res.send(results);
+    }
+});
+
+})
+
+
+
 // console.log(process.env.PORT);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port '+process.env.PORT)
 })
 
